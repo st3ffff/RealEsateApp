5c2880b33e024f7a0691f01a46a4118b15fb4c04
st3ffff <stefan999pro@gmail.com>
2025-04-29 15:19:04 +0300

    Добавяне на JoinUs контролер

---
+++ RealEsateApp/Controllers/JoinUsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Models;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RealEstateApp.Controllers
+{
+    public class JoinUsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadCV(IFormFile cvFile)
+        {
+            if (cvFile != null && cvFile.Length > 0)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "cv", cvFile.FileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await cvFile.CopyToAsync(stream);
+                }
+
+                TempData["Success"] = "Успешно качихте вашето CV!";
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["Error"] = "Моля, прикачете валиден файл.";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
