10f44c0321622a65acf18e2dc0b5276726014966
st3ffff <stefan999pro@gmail.com>
2025-04-29 17:42:40 +0300

    Оправяне на MessagesController спрямо ролята на потребителя

--- RealEsateApp/Controllers/MessagesController.cs
+++ RealEsateApp/Controllers/MessagesController.cs
@@ -62,9 +62,7 @@ namespace RealEstateApp.Controllers
             {
                 var isBroker = await _userManager.IsInRoleAsync(user, "Broker");
 
-                // Ако си брокер, виждаш всички потребители
-                // Ако не си брокер, виждаш само брокери
-                if (User.IsInRole("Broker") || isBroker)
+                if (User.IsInRole("Broker") || User.IsInRole("Admin") || isBroker)
                 {
                     recipients.Add(new SelectListItem
                     {
@@ -75,12 +73,14 @@ namespace RealEstateApp.Controllers
             }
 
             ViewBag.Users = recipients;
-            ViewBag.Placeholder = User.IsInRole("Broker")
+
+            ViewBag.Placeholder = User.IsInRole("Admin")
                 ? "-- Избери потребител --"
-                : "-- Избери брокер --";
+                : User.IsInRole("Broker") ? "-- Избери потребител --" : "-- Избери брокер --";
 
             return View();
         }
+
 
 
 
