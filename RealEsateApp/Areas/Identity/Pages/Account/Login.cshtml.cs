7a026b61ed07e411c29a37a54818bd19b61c05ed
st3ffff <stefan999pro@gmail.com>
2025-04-29 18:06:26 +0300

    Оправяне на Login

--- RealEsateApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ RealEsateApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -129,7 +129,7 @@ namespace RealEsateApp.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Грешен имейл или парола.");
                     return Page();
                 }
             }
